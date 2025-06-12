8d7c0c7438b64799c42d33a21c2dd2b6d6043582
Milson2601 <dudemilind42@gmail.com>
2025-06-12 21:59:00 +0200

    Allcode

---
+++ bookstore_CI/app/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+# This is a chnage for git repo test 
+
+
+
+app = FastAPI()
+
+# ---- Mock Data Storage ----
+fake_books_db = []
+
+# ---- Schemas ----
+class BookCreate(BaseModel):
+    title: str
+    author: str
+
+class BookResponse(BookCreate):
+    id: int
+
+class HealthCheckResponse(BaseModel):
+    message: str
+# ---- Endpoints ----
+
+@app.post("/books/", response_model=BookResponse)
+async def create_book(book: BookCreate):
+    new_book = BookResponse(id=len(fake_books_db) + 1, **book.dict())
+    fake_books_db.append(new_book)
+    return new_book
+
+@app.get("/books/", response_model=List[BookResponse])
+async def get_books():
+    return fake_books_db
+
+@app.get("/health-check", response_model=str)
+async def health_check():
+    return HealthCheckResponse(message="Healthy")
